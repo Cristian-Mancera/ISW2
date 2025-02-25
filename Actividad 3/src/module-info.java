dc08e25e0170a929b79a33c277707fd06d6c7096
Santiago <161547789+santiRojas18@users.noreply.github.com>
2025-02-24 23:33:59 -0500

    Delete Actividad 3/src directory

--- Actividad 3/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module software2_ejercicio1 {
-    
-    requires java.sql;
-
-    opens co.edu.poli.ejemplo1.vista to javafx.graphics, javafx.fxml; 
-    
-    exports co.edu.poli.ejemplo1.controlador;  
-    
-}
