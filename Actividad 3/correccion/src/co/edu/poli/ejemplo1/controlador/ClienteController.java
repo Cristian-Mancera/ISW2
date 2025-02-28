43559b4528274accefa623d20ee5c1a5d2744ac7
Santiago <161547789+santiRojas18@users.noreply.github.com>
2025-02-27 19:16:17 -0500

    Add files via upload

---
+++ Actividad 3/correccion/src/co/edu/poli/ejemplo1/controlador/ClienteController.java
@@ -0,0 +1,38 @@
+package co.edu.poli.ejemplo1.controlador;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import co.edu.poli.ejemplo1.modelo.Cliente;
+import co.edu.poli.ejemplo1.servicios.ClienteImplementacionDAO;
+import co.edu.poli.ejemplo1.servicios.GestionConexion;
+import co.edu.poli.ejemplo1.vista.Main;
+
+public class ClienteController {
+
+	ClienteImplementacionDAO clienteDAO;
+
+	public ClienteController() {
+		try {
+
+			Connection conexion = GestionConexion.obtenerConexion();
+
+			clienteDAO = new ClienteImplementacionDAO();
+			GestionConexion.cerrarConexion();
+
+		} catch (SQLException | IOException e) {
+
+			System.out.println("Error durante la conexión o cierre: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	public String Actioncrear() {
+
+		Cliente c = new Cliente(Main.txtid, Main.txtname);
+
+		return clienteDAO.create(c);
+
+	}
+}
