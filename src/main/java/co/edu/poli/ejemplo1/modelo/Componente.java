7c99ed7899544e8aeb9b323bdca801a8f055b890
Cristian <161549657+Cristian-Mancera@users.noreply.github.com>
2025-03-25 17:25:09 -0500

    Delete src directory

--- src/main/java/co/edu/poli/ejemplo1/modelo/Componente.java
+++
@@ -1,5 +0,0 @@
-package co.edu.poli.ejemplo1.modelo;
-
-interface Componente {
-	void mostrarInformacion();
-}
--- src/main/java/co/edu/poli/ejemplo1/modelo/Prototype.java
+++
@@ -1,7 +0,0 @@
-package co.edu.poli.ejemplo1.modelo;
-
-public interface Prototype {
-	
-	public Prototype clone() throws CloneNotSupportedException;
-
-}
