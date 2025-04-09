edeb65f7753bd2aaf2f99849b7858dbb58194132
santiRojas18 <sanrojas@poligran.edu.co>
2025-04-09 13:17:25 -0500

    arreglos del formulario

--- src/main/java/co/edu/poli/ISW/controlador/ControladorFormulario.java
+++ src/main/java/co/edu/poli/ISW/controlador/ControladorFormulario.java
@@ -1,5 +1,24 @@
 package co.edu.poli.ISW.controlador;
 
+import co.edu.poli.ISW.modelo.Facade;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ControladorFormulario {
+
+	@FXML
+	private Button bttfacade;
+
+	@FXML
+	void facade(ActionEvent event) {
+
+//		Facade facade = new Facade(null, null, null);
+//		ﬁ
+//		facade.GestionarCliente(null, null, null);
+		
+		System.out.println("aqui va el facade");
+
+	}
 
 }
--- src/main/java/co/edu/poli/ISW/vista/App.java
+++ src/main/java/co/edu/poli/ISW/vista/App.java
@@ -4,33 +4,34 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-
 public class App extends Application {
 
-    private static Scene scene;
+	private static Scene scene;
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+	@Override
+	public void start(Stage stage) throws IOException {
+		TabPane root = (TabPane) FXMLLoader.load(getClass().getResource("/co/edu/poli/ISW/vista/formulario.fxml"));
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+	static void setRoot(String fxml) throws IOException {
+		scene.setRoot(loadFXML(fxml));
+	}
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+	private static Parent loadFXML(String fxml) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+		return fxmlLoader.load();
+	}
 
-    public static void main(String[] args) {
-        launch();
-    }
+	public static void main(String[] args) {
+		launch();
+	}
 
 }
